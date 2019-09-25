2fd375b8bbee2ddb2c817af55e1cdf257187fca0
Bruno Martins <bgcngm@gmail.com>
2019-09-26 00:04:01 +0100

    curtana: Move offline charging daemon to new path and update label
    The charger daemon has been moved to /system/bin, so mimic that change
    but keep using the nice deamon. Also the service label got changed to charger
    and daemon is now labeled as charger_exec, therefore update it accordingly.
    
    Change-Id: I93a88aef41f52837fd0be691e414a5319b689208

--- curtana-vendor.mk
+++ curtana-vendor.mk
@@ -18,7 +18,6 @@ PRODUCT_SOONG_NAMESPACES += \
     vendor/redmi/curtana
 
 PRODUCT_COPY_FILES += \
-    vendor/redmi/curtana/proprietary/bin/charger:$(TARGET_COPY_OUT_SYSTEM)/bin/charger \
     vendor/redmi/curtana/proprietary/etc/permissions/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-qti.xml \
     vendor/redmi/curtana/proprietary/etc/permissions/qti_libpermissions.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/qti_libpermissions.xml \
     vendor/redmi/curtana/proprietary/etc/permissions/qti_permissions.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/qti_permissions.xml \
@@ -66,4 +65,5 @@ PRODUCT_PACKAGES += \
     QtiTelephonyService \
     dpmserviceapp \
     ims \
-    qcrilmsgtunnel
+    qcrilmsgtunnel \
+    chargeonlymode
